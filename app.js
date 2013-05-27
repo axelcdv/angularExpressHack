f9482c526c414a3821b9adc63ad0a9422ed613f7
Axel Colin de Verdiere <axel@axelcdv.com>
2013-05-27 17:39:42 +0200

    Updated port for heroku

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.delete('/api/post/:id', api.deletePost);
 app.get('*', routes.index);
 
 // Start server
-
-app.listen(3000, function(){
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
